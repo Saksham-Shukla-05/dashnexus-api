ac3f319b67c0b16809e639c166fad3ea85e4b92f
Saksham-Shukla-05 <get467997@gmail.com>
2025-03-01 14:27:38 +0530

    Add user registration route and controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import globalErrorHandler from "./MiddleWares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
@@ -8,6 +9,8 @@ app.get("/", (req, res) => {
     Message: "Welcome to the application",
   });
 });
+
+app.use("/api/users", userRouter);
 
 //global error handler (Should be written at last after all the routes)
 app.use(globalErrorHandler);
