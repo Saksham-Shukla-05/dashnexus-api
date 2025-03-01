d030d90dc4b6379302badb927da570e925ba1b7a
Saksham-Shukla-05 <get467997@gmail.com>
2025-03-01 15:11:48 +0530

    add user model and validation for registration

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Response, Request } from "express";
 import createHttpError from "http-errors";
 import { body, validationResult } from "express-validator";
+import userModel from "./userModel";
 
+// Validation
 const validateUser = [
   body("name").notEmpty().withMessage("Name is required"),
   body("email").isEmail().withMessage("Invalid email"),
@@ -12,12 +14,24 @@ const validateUser = [
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
     return next(
       createHttpError(400, {
         message: errors.array().map((err) => err.msg),
       })
     );
+  }
+
+  // Database call.
+  const user = await userModel.findOne({ email: req.body.email });
+
+  if (user) {
+    const error = createHttpError(
+      400,
+      "User Already exisit with the provided email "
+    );
+    return next(error);
   }
 
   res.json({
