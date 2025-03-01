345249cbff50e86f3298ee78b0da7c064bde95cf
Saksham-Shukla-05 <get467997@gmail.com>
2025-03-01 13:40:49 +0530

    Added global error handler

---
+++ src/MiddleWares/globalErrorHandler.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { HttpError } from "http-errors";
+import { config } from "../config/config";
+
+const globalErrorHandler = (err: HttpError, req: Request, res: Response) => {
+  const statusCode = err.status || 500;
+  res.status(statusCode).json({
+    message: err.message,
+    errorStack: config.env === "development" ? err.stack : "",
+  });
+};
+
+export default globalErrorHandler;
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import globalErrorHandler from "./MiddleWares/globalErrorHandler";
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -7,4 +9,6 @@ app.get("/", (req, res) => {
   });
 });
 
+//global error handler (Should be written at last after all the routes)
+app.use(globalErrorHandler);
 export default app;
