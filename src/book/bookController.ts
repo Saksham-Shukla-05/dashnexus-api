0d89351d4e11e154f29fd8b2185af7544eb95c80
Saksham-Shukla-05 <get467997@gmail.com>
2025-03-02 14:45:37 +0530

    book creation with file uploads and temporary file cleanup

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -2,10 +2,12 @@ import { NextFunction, Response, Request } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "path";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
 
+import fs from "fs";
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   const files = req.files as { [filename: string]: Express.Multer.File[] };
-
+  const { title, genre } = req.body;
   const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
   const fileName = files.coverImage[0].filename;
   const filePath = path.resolve(
@@ -37,8 +39,22 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
       }
     );
 
-    console.log("bookFileUploadResult", bookFileUploadResult);
-    res.json({});
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "67c2dc0150813bbce9a7ccc5",
+      coverImage: uploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+
+    //delete temp files
+
+    await fs.promises.unlink(filePath);
+    await fs.promises.unlink(bookFilePath);
+
+    res.status(201).json({
+      id: newBook._id,
+    });
   } catch (error) {
     return next(
       createHttpError(500, "Something went wrong while uploading files")
