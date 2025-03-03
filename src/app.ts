863ac460741208c70e3192250253f30c15f8c4b7
Saksham-Shukla-05 <get467997@gmail.com>
2025-03-03 16:40:52 +0530

    Update CORS to allow requests from the frontend domain

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,14 @@ import globalErrorHandler from "./MiddleWares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import cors from "cors";
 import bookRouter from "./book/bookRouter";
+import { config } from "./config/config";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: config.fronted_domain,
+  })
+);
 
 app.use(express.json());
 app.get("/", (req, res) => {
